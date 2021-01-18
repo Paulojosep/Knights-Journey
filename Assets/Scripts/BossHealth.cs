09fccf6cefd46a144bd43136ca3ac6747b28258e
Paulojosep <paulo.jmodesto@htmail.com>
2021-01-18 11:23:46 -0300

    Game Project - Knights Journey

---
+++ Assets/Scripts/BossHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+    public int health = 1000;
+    public GameObject Win;
+    public void TakeDamage(int damage)
+    {
+
+        health -= damage;
+
+        if(health <= 200)
+        {
+            GetComponent<Animator>().SetBool("isAttack2", true);
+        }
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+        Win.SetActive(true);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Fire")
+        {
+            Destroy(other.gameObject);
+            TakeDamage(15);
+        }
+    }
+}
